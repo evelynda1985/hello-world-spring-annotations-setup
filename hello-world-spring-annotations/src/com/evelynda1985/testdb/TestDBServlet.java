83732566d7411ed6cb0deb6c0c5a175dbedcbcb2
Evelynda <engevelynda@gmail.com>
2018-07-12 18:10:32 -0500

    Step 2:  Add to lib -> MySQl conector  create a servlet to test the db  copy paste and update it with your db information  //very important to import this --> import java.sql.*;
     copy and paste spring-config.xml and update it with your db info
     web.xml only paste it
    
     Read comments for more information

---
+++ hello-world-spring-annotations/src/com/evelynda1985/testdb/TestDBServlet.java
@@ -0,0 +1,53 @@
+package com.evelynda1985.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*; 
+
+// very important to import this import java.sql.*;
+
+//En general this is the same process for a test your database connection
+// you can just copy and paste it and update username, password, url and driver in case they are different and print out is optional.
+
+
+@WebServlet("/TestDatabase")
+public class TestDBServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Write your database information here
+		
+		String user = "github";
+		String pass = "github";
+		String url = "jdbc:mysql://localhost:3306/sakila?useSSL=false";//put here the name of the path of database
+		String driver = "com.mysql.jdbc.Driver";
+		
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("Connecting to database: " + url + "\n"); //you can put any message, it is only to verify the connection
+			Class.forName(driver);
+			Connection myConn = DriverManager.getConnection(url, user, pass);
+			out.println("SUCCESS!!!");//you can put any message, it is only to verify the connection
+			myConn.close();
+			
+		} catch (Exception e) {
+			System.out.println("try again problem with the connection"); //you can put any message, it is only to verify the connection
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+
+	}
+
+}
